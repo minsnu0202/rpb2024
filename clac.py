d0d0fc209b4b9bfcc597332b09b11381fb3f2e4f
minsnu0202 <165485851+minsnu0202@users.noreply.github.com>
2024-03-31 12:21:24 +0900

    Update clac.py

--- clac.py
+++ clac.py
@@ -1,3 +1,20 @@
 def main():
+    print("Let's implement division. Type two numbers for x and y")
 
-def add():
+    x = int(input("x > "))
+    y = int(input("y > "))
+    if y!=0 :
+        print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    else : print("Error: cannot divide by zero.")
+
+
+def add(a, b):
+    return a + b
+
+
+# TODO: divide() 함수 정의
+def divide(x,y):
+    return x/y        
+
+if __name__ == "__main__":
+    main()
